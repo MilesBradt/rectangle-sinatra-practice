ec3bc565f87a6008fc91a20504b41e83b970897f
Miles Bradt <smbradtmichael@gmail.com>
2018-11-25 19:32:19 -0800

    basic sinatra working from examples

---
+++ app.rb
@@ -0,0 +1,17 @@
+require('sinatra')
+
+get('/test') do
+  @description = "This application will determine whether a rectangle is a square or not."
+  erb(:input)
+end
+
+get('/output') do
+  @length = params.fetch("length")
+  @width = params.fetch("width")
+  if @length == @width
+    @rectangle = "This is a square."
+  else
+    @rectangle = "This is not a square."
+  end
+  erb(:output)
+end
